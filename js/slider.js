8f4a62a02652ae044f6cf245adcc4964edb038f3
asifhjahid <dhrubo8211diu@gmail.com>
2021-09-16 04:59:32 +0600

    latest product section

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,7 @@
 // Glide Js Carousel
 
 const slider1 = document.querySelector('#glide-1');
-
+const slider2 = document.querySelector('#glide_2')
 
 //Hero 
 
@@ -11,8 +11,32 @@ if(slider1){
         startAt:0,
         autoplay:3000,
         hoverpause:true,
-        preView:1,
+        perView:1,
         animationDuration:800,
         animationTimingFunc:'linear',
+    }).mount();
+}
+
+
+
+// Latest Products Section
+
+if(slider1){
+    new Glide(slider2,{
+        type:"carousel",
+        startAt:0,
+        autoplay:2000,
+        hoverpause:true,
+        perView:4,
+        animationDuration:800,
+        animationTimingFunc:'cubic-bezier(.165,.840,.44,.1)',
+        breakpoints:{
+            1200:{
+                perView:3,
+            },
+            768:{
+                perView:2,
+            },
+        },
     }).mount();
 }
