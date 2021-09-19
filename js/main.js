e1d72f92b9028e8f3d45dc54ff2dd55db61b749f
asifhjahid <dhrubo8211diu@gmail.com>
2021-09-19 19:52:45 +0600

    customer feedback section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// import Swiper from 'swiper/swiper-bundle.esm.js';
+// import 'swiper/swiper-bundle.css';
+
 const hamburer = document.querySelector(".hamburger");
 const navList = document.querySelector(".nav-list");
 
@@ -6,3 +9,73 @@ if (hamburer) {
     navList.classList.toggle("open");
   });
 }
+
+
+
+
+//Deal of the Week
+
+const daysE1 = document.querySelector("#days");
+const hoursE1 = document.querySelector("#hours");
+const minutesE1 = document.querySelector("#minutes");
+const secondsE1 = document.querySelector("#seconds");
+
+const newYears = " 2";
+
+const date = new Date();
+const l = date.getDate()+3;
+console.log(l);
+
+function countDown(){
+    // const newYearsDate = new Date(l);
+    // console.log(newYearsDate);
+    const currentDate = new Date();
+    // const lal = currentDate.getDate();
+    // console.log(lal);
+    
+    console.log();
+    const totalSeconds = (l - currentDate)/1000;
+    console.log(totalSeconds);
+    
+    const days = Math.floor(totalSeconds/3600/24);
+    const hours = Math.floor(totalSeconds/3600)%24;
+    const minutes = Math.floor(totalSeconds/60)%60;
+    const seconds = Math.floor(totalSeconds%60);
+    
+    daysE1.innerHTML = days;
+    hoursE1.innerHTML = hours;
+    minutesE1.innerHTML = minutes;
+    secondsE1.innerHTML = seconds;
+
+   
+}
+
+//initial call
+// countDown();
+// setInterval(countDown,1000);
+
+
+
+
+// Customer Reviews
+
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay:{
+    delay:3000,
+    disableOnInteraction:false
+  }
+});
